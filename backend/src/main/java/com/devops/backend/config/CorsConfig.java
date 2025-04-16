366ea7a85e1a0eca4f460ff6344395cd99765a2f
Vikas <xanderbilla@gmail.com>
2025-04-17 00:18:22 +0530

    docker compose (local) ✅

--- backend/src/main/java/com/devops/backend/config/CorsConfig.java
+++ backend/src/main/java/com/devops/backend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://13.235.134.111:3000")
+                .allowedOrigins("http://13.235.134.111:3000", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

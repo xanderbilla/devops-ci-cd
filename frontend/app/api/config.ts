83f3e50aa5e8f0e55ac394da74fd47ea1b7ba987
Vikas <xanderbilla@gmail.com>
2025-04-22 10:09:15 +0530

    config updated

--- frontend/app/api/config.ts
+++ frontend/app/api/config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
-  BASE_URL: "http://65.0.133.105:8500",
+  BASE_URL: "http://13.235.247.107:8500",
+  // BASE_URL: "http://localhost:8080",
   ENDPOINTS: {
     HEALTH: "/health",
     PERSONS: "/api/persons",
